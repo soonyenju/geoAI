9bc79eb0f76ee658c2c6cffd620354b15d1a8272
Songyan Zhu <zhusy93@gmail.com>
2025-03-26 10:28:41 +0000

    Update functions.py

--- geoAI/functions.py
+++ geoAI/functions.py
@@ -1,4 +1,11 @@
-import numpy as np
+import matplotlib as plt
 
-def func(x):
-    return x**2
+def show_city_boundary(shp, country, buffer = 0.1, fc = '#ff7f0e'):
+    shp.plot()
+    ax = plt.gca()
+    country.plot(ax = ax, fc = '#1f77b4', alpha = 0.2)
+    ax.set_xlim(shp.bounds.minx.item() - buffer, shp.bounds.maxx.item() + buffer)
+    ax.set_ylim(shp.bounds.miny.item() - buffer, shp.bounds.maxy.item() + buffer)
+    return ax
+    
+
