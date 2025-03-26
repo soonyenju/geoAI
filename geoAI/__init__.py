faf1d4277582ae95c8beb224d5340514e06527a4
Songyan Zhu <zhusy93@gmail.com>
2025-03-26 11:03:17 +0000

    Update __init__.py

--- geoAI/__init__.py
+++ geoAI/__init__.py
@@ -1,3 +1,4 @@
 from . import functions
+from shapely.geometry import Point, Polygon, LineString, MultiPoint, MultiLineString, MultiPolygon
 
-__all__ = ["functions"]
+__all__ = ["functions", "Point", "Polygon", "LineString", "MultiPoint", "MultiLineString", "MultiPolygon"]
