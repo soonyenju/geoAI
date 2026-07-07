29ff5359a736a1f9f8ceccfec4feadf876a45358
Songyan Zhu <Songyan.Zhu@soton.ac.uk>
2026-07-07 12:30:37 +0100

    Update functions.py

--- geoAI/functions.py
+++ geoAI/functions.py
@@ -72,9 +72,9 @@ def closest_point2line(point, curves):
 
 
 
-from scigeo.meteo import saturation_vapor_pressure, specific_humidity2vapor_pressure
-from scitbx.utils import *
-from scigeo.geoface import load_tif
+from geoxai.geo.meteo import saturation_vapor_pressure, specific_humidity2vapor_pressure
+from geoxai.utils import *
+from geoxai.geo.geoface import load_tif
 
 def load_era5(p_ERA5):
     era5 = load_tif(p_ERA5, band_names = [
